6ab3705c71a4c7b900ac6a4c26cf9b28babda1e3
m.pritugin <m.pritugin@corp.mail.ru>
2019-06-02 22:30:24 +0300

    FIXXXX

---
+++ DFA_init.py
@@ -0,0 +1,23 @@
+from CoreBot import DFA, PF
+
+
+def DFA_init():
+    DFA.add_state("some_words_1")
+    DFA.add_state("some_words_2")
+    DFA.add_state("input_phone")
+    DFA.add_state("input_email")
+    DFA.add_state("input_phone_error")
+    DFA.add_state("input_email_error")
+    DFA.add_state("end_input")
+    DFA.add_state("problem_1")
+    DFA.add_state("endprob_1")
+    DFA.add_state("problem_2")
+    DFA.add_state("endprob_2")
+    DFA.add_state("problem_3")
+    DFA.add_state("endprob_3")
+    DFA.add_state("problem_4")
+    DFA.add_state("endprob_4")
+
+
+
+
