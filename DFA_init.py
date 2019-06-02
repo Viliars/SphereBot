e1b15884ab68461204fb1786e0c40af4eff78482
Morgan-iv <ivushkinalex@gmail.com>
2019-06-02 23:29:46 +0300

    Add regexes

--- DFA_init.py
+++ DFA_init.py
@@ -18,26 +18,29 @@ def DFA_init():
     DFA.add_state("problem_4")
     DFA.add_state("endprob_4")
 
+    reph = r'^ *(\+? *7|8)? *-? *(\(9\d\d\)|9\d\d) *( *-? *\d){7} *$'
+    reem = r'''(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])'''
+
     # -------------------------
     # DFA.add_transition(regex, source, dest, func)
-    DFA.add_transition(None, "some_words_1", "some_words_2", PF.any)
-    DFA.add_transition(None, "some_words_2", "input_phone", PF.any)
-    DFA.add_transition(None, "input_phone", "input_email", PF.parse_phone)
-    DFA.add_transition(None, "input_phone", "input_phone_error", PF.any)  # ERROR
-    DFA.add_transition(None, "input_phone_error", "input_phone_error", PF.any)  # ERROR
-    DFA.add_transition(None, "input_phone_error", "input_email", PF.parse_phone)  # OK
-    DFA.add_transition(None, "input_email", "end_input", PF.parse_email)
-    DFA.add_transition(None, "input_email", "input_email_error", PF.any)  # ERROR
-    DFA.add_transition(None, "input_email_error", "input_email_error", PF.any)  # ERROR
-    DFA.add_transition(None, "input_email_error", "end_input", PF.parse_email)  # OK
+    DFA.add_transition('.*', "some_words_1", "some_words_2", PF.any)
+    DFA.add_transition('.*', "some_words_2", "input_phone", PF.any)
+    DFA.add_transition(reph, "input_phone", "input_email", PF.parse_phone)
+    DFA.add_transition('.*', "input_phone", "input_phone_error", PF.any)  # ERROR
+    DFA.add_transition(reph, "input_phone_error", "input_email", PF.parse_phone)  # OK
+    DFA.add_transition('.*', "input_phone_error", "input_phone_error", PF.any)  # ERROR
+    DFA.add_transition(reem, "input_email", "end_input", PF.parse_email)
+    DFA.add_transition('.*', "input_email", "input_email_error", PF.any)  # ERROR
+    DFA.add_transition(reem, "input_email_error", "end_input", PF.parse_email)  # OK
+    DFA.add_transition('.*', "input_email_error", "input_email_error", PF.any)  # ERROR
 
     # ----- Задачи -----
-    DFA.add_transition(None, "end_input", "problem_1", PF.any)
-    DFA.add_transition(None, "problem_1", "endprob_1", PF.any_save)
-    DFA.add_transition(None, "endprob_1", "problem_2", PF.any)
-    DFA.add_transition(None, "problem_2", "endprob_2", PF.any_save)
-    DFA.add_transition(None, "endprob_2", "problem_3", PF.any)
-    DFA.add_transition(None, "problem_3", "endprob_3", PF.any_save)
-    DFA.add_transition(None, "endprob_3", "problem_4", PF.any)
-    DFA.add_transition(None, "problem_4", "endprob_4", PF.any_save)
-    DFA.add_transition(None, "endprob_4", "endprob_4", PF.any)
+    DFA.add_transition('.*', "end_input", "problem_1", PF.any)
+    DFA.add_transition('.*', "problem_1", "endprob_1", PF.any_save)
+    DFA.add_transition('.*', "endprob_1", "problem_2", PF.any)
+    DFA.add_transition('.*', "problem_2", "endprob_2", PF.any_save)
+    DFA.add_transition('.*', "endprob_2", "problem_3", PF.any)
+    DFA.add_transition('.*', "problem_3", "endprob_3", PF.any_save)
+    DFA.add_transition('.*', "endprob_3", "problem_4", PF.any)
+    DFA.add_transition('.*', "problem_4", "endprob_4", PF.any_save)
+    DFA.add_transition('.*', "endprob_4", "endprob_4", PF.any)
