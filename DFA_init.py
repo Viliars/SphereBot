1d7e26100425b1841056c217f6596eee5c258112
m.pritugin <m.pritugin@corp.mail.ru>
2019-06-02 22:41:50 +0300

    FIXXXXX

--- DFA_init.py
+++ DFA_init.py
@@ -18,6 +18,26 @@ def DFA_init():
     DFA.add_state("problem_4")
     DFA.add_state("endprob_4")
 
+    # -------------------------
+    # DFA.add_transition(regex, source, dest, func)
+    DFA.add_transition(None, "some_words_1", "some_words_2", PF.any)
+    DFA.add_transition(None, "some_words_2", "input_phone", PF.any)
+    DFA.add_transition(None, "input_phone", "input_email", PF.parse_phone)
+    DFA.add_transition(None, "input_phone", "input_phone_error", PF.any)  # ERROR
+    DFA.add_transition(None, "input_phone_error", "input_phone_error", PF.any)  # ERROR
+    DFA.add_transition(None, "input_phone_error", "input_email", PF.parse_phone)  # OK
+    DFA.add_transition(None, "input_email", "end_input", PF.parse_email)
+    DFA.add_transition(None, "input_email", "input_email_error", PF.any)  # ERROR
+    DFA.add_transition(None, "input_email_error", "input_email_error", PF.any)  # ERROR
+    DFA.add_transition(None, "input_email_error", "end_input", PF.parse_email)  # OK
 
-
-
+    # ----- Задачи -----
+    DFA.add_transition(None, "end_input", "problem_1", PF.any)
+    DFA.add_transition(None, "problem_1", "endprob_1", PF.any)
+    DFA.add_transition(None, "endprob_1", "problem_2", PF.any)
+    DFA.add_transition(None, "problem_2", "endprob_2", PF.any)
+    DFA.add_transition(None, "endprob_2", "problem_3", PF.any)
+    DFA.add_transition(None, "problem_3", "endprob_3", PF.any)
+    DFA.add_transition(None, "endprob_3", "problem_4", PF.any)
+    DFA.add_transition(None, "problem_4", "endprob_4", PF.any)
+    DFA.add_transition(None, "endprob_4", "endprob_4", PF.any)
